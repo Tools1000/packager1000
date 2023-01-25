e8838b711b5a25f62f3030d40d72aaeb55d4be68
Alexander Kerner <alexkerner1000@gmail.com>
2023-01-25 17:58:19 +0100

    Add test case

---
+++ java/src/test/java/javacodesign/CodeSignerTest.java
@@ -0,0 +1,31 @@
+package javacodesign;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+class CodeSignerTest {
+
+    private String identity;
+
+    @BeforeEach
+    void setUp() {
+        identity = "Developer ID Application: John Doe (1234567890)";
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test01() throws IOException {
+        CodeSigner codeSigner = new CodeSigner(identity, Paths.get("../test/resources/DrKodi.app"), Paths.get("../test/resources/DrKodi.app/Contents/MacOS/launcher-macosx-x86_64.sh"), Paths.get("../entitlements-jvm.plist"), Paths.get("../entitlements-launcher.plist"));
+
+        codeSigner.removeSignature();
+        codeSigner.sign();
+//        codeSigner.removeSignature();
+    }
+}
